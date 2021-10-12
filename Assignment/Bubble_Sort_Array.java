edf9efef5015f09f9bb5f2dee3e410576ed2aca9
Kunalsjcodes <kunal.sj01@gmail.com>
2021-10-12 21:52:43 +0530

    Bubble Sort

---
+++ Assignment/Bubble_Sort_Array.java
@@ -0,0 +1,36 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Bubble_Sort_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i] + " ");
+		}
+	}
+
+	public static void Sort(int[] arr) {
+
+		for (int pass = 1; pass < arr.length; pass++) {
+			for (int i = 0; i < arr.length - pass; i++) {
+				if (arr[i] > arr[i + 1]) {
+					int t = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = t;
+				}
+
+			}
+
+		}
+
+	}
+}
