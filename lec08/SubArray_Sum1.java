5925da7f037f80ffe3364d7bfcbd532eec2e2a85
Kunalsjcodes <kunal.sj01@gmail.com>
2021-10-17 00:17:35 +0530

    lec08

---
+++ lec08/SubArray_Sum1.java
@@ -0,0 +1,27 @@
+package lec08;
+
+public class SubArray_Sum1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { -3, 4, 5, -7, 8 };
+		System.out.println(max_sum(arr));
+
+	}
+
+	public static int max_sum(int[] arr) {
+		int ans = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				int sum = 0;
+				for (int k = i; k <= j; k++) {
+					sum += arr[k];
+				}
+				ans = Math.max(sum, ans);
+			}
+		}
+		return ans;
+
+	}
+
+}
---
+++ lec08/SubArray_Sum3_KadenesAlgo.java
@@ -0,0 +1,27 @@
+package lec08;
+
+public class SubArray_Sum3_KadenesAlgo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { -3, -4, -5, -7, -8 };
+		System.out.println(Kadenes(arr));
+
+	}
+
+	public static int Kadenes(int[] arr) {
+		int ans = Integer.MIN_VALUE;
+		int curr_sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			curr_sum += arr[i];
+			ans = Math.max(ans, curr_sum);
+			if (curr_sum < 0) {
+				curr_sum = 0;
+			}
+
+		}
+		return ans;
+
+	}
+
+}
