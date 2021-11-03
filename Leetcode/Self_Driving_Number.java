a830bf6725db635b5687ba8c99b1208fdf448722
Kunalsjcodes <kunal.sj01@gmail.com>
2021-11-03 20:30:03 +0530

    leetcode

---
+++ Leetcode/Self_Driving_Number.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class Self_Driving_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// https://leetcode.com/problems/self-dividing-numbers/
+	}
+
+//	public List<Integer> selfDividingNumbers(int left, int right) {
+//		List<Integer> ans = new ArrayList();
+//		for (int n = left; n <= right; ++n) {
+//			if (selfDividing(n))
+//				ans.add(n);
+//		}
+//		return ans;
+//	}
+//
+//	public boolean selfDividing(int n) {
+//		for (char c : String.valueOf(n).toCharArray()) {
+//			if (c == '0' || (n % (c - '0') > 0))
+//				return false;
+//		}
+//		return true;
+//	}
+}
