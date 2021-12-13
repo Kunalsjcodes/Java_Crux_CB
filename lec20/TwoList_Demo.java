d90aa8eba223def7afd2118ac58be8208a40e30e
Kunalsjcodes <kunal.sj01@gmail.com>
2021-12-13 23:23:25 +0530

    lec 20

---
+++ lec20/TwoList_Demo.java
@@ -0,0 +1,13 @@
+package lec20;
+import java.util.*;
+public class TwoList_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<ArrayList<String>> list = new ArrayList<>();
+		list.add(new ArrayList<>());
+		list.add(new ArrayList<>());
+		System.out.println(list);
+	}
+
+}
