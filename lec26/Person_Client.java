87b0d59473bd5cbd191ac73e08d95ef4c3b990b7
Kunalsjcodes <kunal.sj01@gmail.com>
2022-01-09 18:38:08 +0530

    oops

---
+++ lec26/Person_Client.java
@@ -0,0 +1,17 @@
+package lec26;
+
+public class Person_Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Person p = new Person();
+		System.out.println("hey");
+		p.setAge(10);
+		System.out.println(p.getAge());
+		// System.out.println(p.getName());
+		System.out.println("bye");
+
+	}
+
+}
