e34836d5d401cb15040d986e50677c528befe860
Kunalsjcodes <kunal.sj01@gmail.com>
2022-01-27 19:09:37 +0530

    Leetcode

---
+++ Leetcode/Nim_Game.java
@@ -0,0 +1,13 @@
+package Leetcode;
+
+public class Nim_Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// https://leetcode.com/problems/nim-game/submissions/
+	}
+
+	public boolean canWinNim(int n) {
+		return (n % 4 != 0);
+	}
+}
