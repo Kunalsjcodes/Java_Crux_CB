a6b10e1265e659b2db21815465d9d2363fb1b058
Kunalsjcodes <kunal.sj01@gmail.com>
2022-02-10 19:56:20 +0530

    Tree

---
+++ lec36/BST_Client.java
@@ -0,0 +1,12 @@
+package lec36;
+
+public class BST_Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+		BinarySearchTree bst = new BinarySearchTree(arr);
+		bst.PreOrder();
+
+	}
+}
