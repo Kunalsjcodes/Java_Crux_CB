86d23d60c76687c3d81c9b2abd15310ada2f8b85
Kunalsjcodes <kunal.sj01@gmail.com>
2022-03-09 22:32:29 +0530

    Leetcode

---
+++ Leetcode/Distribute_Candies.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class Distribute_Candies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// https://leetcode.com/problems/distribute-candies/
+	}
+
+	public int distributeCandies(int[] candyType) {
+		int uniqueCandies = 0;
+		for (int i = 0; i < candyType.length; i++) {
+			boolean isUnique = true;
+			for (int j = 0; j < i; j++) {
+				if (candyType[i] == candyType[j]) {
+					isUnique = false;
+					break;
+				}
+			}
+			if (isUnique) {
+				uniqueCandies++;
+			}
+		}
+		return Math.min(uniqueCandies, candyType.length / 2);
+	}
+}
