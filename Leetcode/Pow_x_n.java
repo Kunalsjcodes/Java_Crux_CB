7ec0ff465a0e65f58a57feb1fda98dc04e521618
Kunalsjcodes <kunal.sj01@gmail.com>
2022-03-10 23:07:08 +0530

    Leetcode

---
+++ Leetcode/Pow_x_n.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class Pow_x_n {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// https://leetcode.com/problems/powx-n/
+	}
+
+	public double myPow(double x, int n) {
+		double s = Math.pow(x, n);
+		return s;
+	}
+}
